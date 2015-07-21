343d54667728dfa47bbebf2e15aadcaf40d4d665
nickbaldwin <nick.with.a.z@gmail.com>
2015-07-21 16:42:30 +1000

    initial refactor Main

--- src/datamanagement/Main.java
+++ src/datamanagement/Main.java
@@ -1,4 +1,9 @@
 package datamanagement;
 
 public class Main {
-    public static void main(String[] p) {new cgCTL().execute();}    }
+  public static void main(String[] args) {
+      new cgCTL().execute();
+  }
+
+
+}
