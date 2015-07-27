076391009c3914206e0355704a717c2b09eb0e87
rcallow <rcallow87@gmail.com>
2015-07-27 20:24:54 +1000

    Changed variable ln to studentLastName in Student.java

--- src/datamanagement/Student.java
+++ src/datamanagement/Student.java
@@ -2,11 +2,11 @@ package datamanagement;
 
 public class Student implements IStudent {
   private Integer studentId; private String studentFirstName;
-  private String ln;
+  private String studentLastName;
   private StudentUnitRecordList su;
 
   public Student( Integer id, String studentFirstName, String ln, StudentUnitRecordList su ) { this.studentId = id; this.studentFirstName = studentFirstName;
-  this.ln = ln;this.su = 
+  this.studentLastName = ln;this.su = 
       su == null ? new StudentUnitRecordList() : 
         su;
   }
@@ -19,11 +19,11 @@ public class Student implements IStudent {
     this.studentFirstName = firstName; }
 
   public String getLastName() { 
-    return ln; }
+    return studentLastName; }
   public void setLastName( String lastName ) { 
 
 
-    this.ln = lastName; }
+    this.studentLastName = lastName; }
 
   public void addUnitRecord( IStudentUnitRecord record ) { su.add(record); }
   public IStudentUnitRecord getUnitRecord( String unitCode ) {
