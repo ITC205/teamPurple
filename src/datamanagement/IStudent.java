48b260a7a2da5ce6fa6dfb6b658c87a5c3dc8598
rcallow <rcallow87@gmail.com>
2015-07-30 12:42:49 +1000

    StudentProxy.java: Corrected to one statement per line

--- src/datamanagement/IStudent.java
+++ src/datamanagement/IStudent.java
@@ -2,14 +2,14 @@ package datamanagement;
 
 public interface IStudent
 {
-    public Integer getID();
-    public String getFirstName();
-    public String getLastName();
-    public StudentUnitRecordList getUnitRecords();
-    
-    public void setFirstName(String firstName);
-    public void setLastName(String lastName);
+  public Integer getID();
+  public String getFirstName();
+  public String getLastName();
+  public StudentUnitRecordList getUnitRecords();
 
-    public void addUnitRecord( IStudentUnitRecord record );
-    public IStudentUnitRecord getUnitRecord( String unitCode );
+  public void setFirstName(String firstName);
+  public void setLastName(String lastName);
+
+  public void addUnitRecord( IStudentUnitRecord record );
+  public IStudentUnitRecord getUnitRecord( String unitCode );
 }
