52bef53a9c820905f7c83c2f8e5705ce7f916e51
rcallow <rcallow87@gmail.com>
2015-07-30 14:38:03 +1000

    StudentProxy.java: Corrected indentation to 2 spaces

--- src/datamanagement/StudentProxy.java
+++ src/datamanagement/StudentProxy.java
@@ -2,56 +2,56 @@ package datamanagement;
 
 public class StudentProxy implements IStudent 
 {
-private Integer studentId;
-private String firstName;
-    private String lastName;
-    private StudentManager lI;
-    
-    public StudentProxy( Integer studentId, String firstName, String lastName) 
-    {
-        this.studentId = studentId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-this.lI = StudentManager.get();
-}
+  private Integer studentId;
+  private String firstName;
+  private String lastName;
+  private StudentManager lI;
 
-    public Integer getID() 
-    { 
-      return studentId; 
-}
-    
-public String getFirstName() 
-{ 
-        return firstName; 
-        }
+  public StudentProxy( Integer studentId, String firstName, String lastName) 
+  {
+    this.studentId = studentId;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.lI = StudentManager.get();
+  }
 
-    public String getLastName() 
-    { 
-return lastName; 
-}
-    
-public void setFirstName(String firstName) 
-{
+  public Integer getID() 
+  { 
+    return studentId; 
+  }
+
+  public String getFirstName() 
+  { 
+    return firstName; 
+  }
+
+  public String getLastName() 
+  { 
+    return lastName; 
+  }
+
+  public void setFirstName(String firstName) 
+  {
     lI.getStudent(studentId).setFirstName(firstName);
-    }
+  }
 
-    public void setLastName(String lastName) 
-    {
-        lI.getStudent(studentId).setLastName(lastName);
-        }
+  public void setLastName(String lastName) 
+  {
+    lI.getStudent(studentId).setLastName(lastName);
+  }
 
-    public void addUnitRecord(IStudentUnitRecord record) 
-    {
-        lI.getStudent(studentId).addUnitRecord(record);
-        }
-    
-        public IStudentUnitRecord getUnitRecord(String unitCode) 
-        {
-return lI.getStudent(studentId).getUnitRecord(unitCode);
-}
+  public void addUnitRecord(IStudentUnitRecord record) 
+  {
+    lI.getStudent(studentId).addUnitRecord(record);
+  }
 
-public StudentUnitRecordList getUnitRecords() 
-{ 
-  return lI.getStudent(studentId).getUnitRecords();
+  public IStudentUnitRecord getUnitRecord(String unitCode) 
+  {
+    return lI.getStudent(studentId).getUnitRecord(unitCode);
+  }
+
+  public StudentUnitRecordList getUnitRecords() 
+  { 
+    return lI.getStudent(studentId).getUnitRecords();
   }
 }
