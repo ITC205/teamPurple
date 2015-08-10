d9f39e2bf4bcfeb76117d38ca3df320bd1d14c56
nickbaldwin <nick.with.a.z@gmail.com>
2015-08-10 11:19:53 +1000

    XMLManager getInstance() and TODOs

--- src/datamanagement/XMLManager.java
+++ src/datamanagement/XMLManager.java
@@ -47,7 +47,7 @@ public class XMLManager
    * Returns the sole xmlManager instance (following the singleton pattern).
    * @return XMLManager instance responsible for managing data from xml file.
    */
-  public static XMLManager getXML()
+  public static XMLManager getInstance()
   {
     if (instance_ == null) {
       instance_ = new XMLManager();
@@ -69,6 +69,19 @@ public class XMLManager
   }
 
 
+  /**
+   * Sets the object which holds the transformed set of data loaded from
+   * the xml file.
+   * Private access restricts use to the XMLManger instance itslef.
+   * @param document Document Holds the transformed set of data constructed by
+   * SAXBuilder expanding entities from the xml file.
+   */
+  private void setDocument(Document document)
+  {
+    this.document_ = document;
+  }
+
+
 
   //===========================================================================
   // Methods
@@ -79,23 +92,31 @@ public class XMLManager
    */
   public void initialize()
   {
+    // TODO: prefer a AppProperties.getInstance().getXmlFileName();
     String xmlFileName = AppProperties.getInstance().getProperties()
-                                      .getProperty( "XMLFILE" );
+                                      .getProperty("XMLFILE");
 
     try {
       SAXBuilder saxBuilder = new SAXBuilder();
       saxBuilder.setExpandEntities(true);
-      document_ = saxBuilder.build(xmlFileName);
+      Document document = saxBuilder.build(xmlFileName);
+      this.setDocument(document);
     }
     // TODO: name exception more specifically?
     catch (JDOMException exception) {
-      System.err.printf("%s", "DBMD: XMLManager : initialize : caught JDOMException\n");
-      throw new RuntimeException("DBMD: XMLManager : initialize : JDOMException");
+      // TODO: message, refactor logging and throw
+      System.err.printf("%s", "DBMD: XMLManager : initialize : caught " +
+                        "JDOMException\n");
+      throw new RuntimeException("DBMD: XMLManager : initialize : " +
+                                 "JDOMException");
     }
     // TODO: name exception more specifically?
     catch (IOException exception) {
-      System.err.printf("%s", "DBMD: XMLManager : initialize : caught IOException\n");
-      throw new RuntimeException("DBMD: XMLManager : initialize : IOException");
+      // TODO: message, refactor logging and throw
+      System.err.printf("%s", "DBMD: XMLManager : initialize : caught " +
+                        "IOException\n");
+      throw new RuntimeException("DBMD: XMLManager : initialize : " +
+                                 "IOException");
     }
   }
 
@@ -106,16 +127,21 @@ public class XMLManager
    */
   public void saveDocument()
   {
+    // TODO: see initialize;
     String xmlFileName = AppProperties.getInstance()
                                   .getProperties().getProperty("XMLFILE");
+
     try (FileWriter fileWriter = new FileWriter(xmlFileName)) {
       XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
-      xmlOutputter.output( document_, fileWriter );
+      xmlOutputter.output(this.getDocument(), fileWriter);
       fileWriter.close();
     }
     catch (IOException exception) {
-      System.err.printf( "%s\n", "DBMD : XMLManager : saveDocument : Error saving XML to " + xmlFileName);
-      throw new RuntimeException("DBMD: XMLManager : saveDocument : error writing to file");
+      // TODO: message, refactor logging and throw
+      System.err.printf("%s\n", "DBMD : XMLManager : saveDocument : Error " +
+                        "saving XML to " + xmlFileName);
+      throw new RuntimeException("DBMD: XMLManager : saveDocument : error " +
+                                 "writing to file");
     }
   }
 
