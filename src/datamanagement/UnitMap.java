8244bc40c1fd875b5af53610acb8d55c98ee3e2f
nickbaldwin <nick.with.a.z@gmail.com>
2015-08-21 15:46:24 +1000

    updated javadoc in UnitMap

--- src/datamanagement/UnitMap.java
+++ src/datamanagement/UnitMap.java
@@ -1,7 +1,7 @@
 package datamanagement;
 
 /**
- * Stores units (unit proxy instances) in a hashMap
+ * Stores collection of units (UnitProxy instances) in a HashMap
  * using the unit code (String) as the identifier.
  */
 public class UnitMap extends java.util.HashMap<String, IUnit>
