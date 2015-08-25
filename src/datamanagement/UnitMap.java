a49c9410ae7b5e9e7420d36c63bbc4e534bc67f8
nickbaldwin <nick.with.a.z@gmail.com>
2015-08-25 15:32:08 +1000

    R3.1 imported HashMap into UnitMap and StudentMap

--- src/datamanagement/UnitMap.java
+++ src/datamanagement/UnitMap.java
@@ -1,9 +1,11 @@
 package datamanagement;
 
+import java.util.HashMap;
+
 /**
  * Stores collection of units (UnitProxy instances) in a HashMap
  * using the unit code (String) as the identifier.
  */
-public class UnitMap extends java.util.HashMap<String, IUnit>
+public class UnitMap extends HashMap<String, IUnit>
 {
 }
